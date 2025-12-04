6ac1aa266005948b65569e9b15d1bf8b5680cedf
shalfe86 <halfenst@gmail.com>
2025-12-03 21:54:30 -0500

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/nfl-stats-website/', // This must match your repository name exactly
+})
